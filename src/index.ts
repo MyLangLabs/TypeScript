126b05951b339de77e668cfb0330d0a465389365
[nyun-nye] <[2001_11_7@naver.com]>
2024-09-26 01:30:50 +0900

    🚩: number 타입 파라미터

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
 // src/index.ts
-const message: string = "Hello, TypeScript!";
-console.log(message);
+
+function add(num1:number, num2:number){ // 타입 정의
+  console.log(num1 + num2);
+}
+
+add(1,2);
+
+// 다른 타입 사용시 에러 발생
+// add("hello", "world"); 
