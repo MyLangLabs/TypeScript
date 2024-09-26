1f898d7e227b907fb85cd01ab06f5381d50ac21c
[nyun-nye] <[2001_11_7@naver.com]>
2024-09-26 18:02:50 +0900

    🚩 : extens 키워드 사용

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,24 @@ interface Car{
   start(): void;
 }
 
+// extends 키워드 사용
+interface Benz extends Car{
+  door: number;
+  stop(): void;
+}
+
+const benz: Benz = {
+  door: 5,
+  stop(){
+    console.log('stop');
+  },
+  color: 'black',
+  wheels: 4,
+  start(){
+    console.log('go..')
+  }
+}
+
 class Bmw implements Car{
   color;
   wheels = 4;
