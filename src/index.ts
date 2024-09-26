98050cb628ecfb53f90467d0834ee26339282c5a
[nyun-nye] <[2001_11_7@naver.com]>
2024-09-27 00:31:07 +0900

    🚩 : 문자형 리터럴 타입

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,19 @@
-// src/index.ts
-const message: string = "Hello, TypeScript!";
-console.log(message);
+// Literal Types
+
+// const(변하지 않는 값), let(변할 수 있는 값) 선언
+// 정해진 string 값을 가진 것 => 문자열 리터럴 타입
+const userName1 = "Bob"; 
+let userName2 = "Tom";
+
+// 문자열 리터럴 타입
+type Job = "police" | "developer" | "teacher";
+
+interface User{
+  name: string;
+  job: Job;
+}
+
+const user:User = {
+  name: "Bob",
+  job: "developer"
+}
