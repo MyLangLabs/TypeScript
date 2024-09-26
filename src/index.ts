f42761a97a02502356d1ebd933aadb3a1884b87a
[nyun-nye] <[2001_11_7@naver.com]>
2024-09-26 16:35:57 +0900

    🚩: 객체 생성

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
-// src/index.ts
-const message: string = "Hello, TypeScript!";
-console.log(message);
+// user 객체 생성
+let user:object; // 객체는 object 타입
+
+user = {
+  name: 'xx',
+  age: 30
+}
+
+// console.log(user.name) // 에러 발생
