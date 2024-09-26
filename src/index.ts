e17181f1c9fb0b7d4f82167825a7fab42f7b15d0
[nyun-nye] <[2001_11_7@naver.com]>
2024-09-26 12:07:30 +0900

    🚩: Tuple 타입 사용

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,8 @@ let car:string = 'bmw';
 // 배열의 2가지 타입
 let a:number[] = [1,2,3];
 let a2:Array<number> = [1,2,3];
+
+// 튜플 (Tuple)
+let b:[string, number];
+let b2:[string, number, boolean]; // n개 가능
+
