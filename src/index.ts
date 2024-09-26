0482b904c8c08981c058afc5e333906dab7ffd6b
[nyun-nye] <[2001_11_7@naver.com]>
2024-09-26 20:57:07 +0900

    🚩 : 선택적 매개변수+필수 매개변수

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,23 @@
 // 함수
 
-function hello(name?: string){ 
-  // 함수 파라미터로 optional 가능 => 선택적 매개변수
-  return `Hello, ${name || "world"}`;
+// 선택적 매개변수와 필수 매개변수를 바꾸면 안된다.
+function hello(name: string, age?:number):string{ 
+  if (age !== undefined)
+    return `Hello, ${name}. You are ${age}.`;
+  else
+    return `Hello, ${name}`;
 }
 
-// hello와 모습이 같은 함수
-function hello2(name = `world`){
-  return `Hello ${name}`;
+// optional이 앞에 오게 하고 싶으면 undefined를 추가한다.
+function hello2(age: number|undefined, name: string):string{ 
+  if (age !== undefined)
+    return `Hello, ${name}. You are ${age}.`;
+  else
+    return `Hello, ${name}`;
 }
+
+console.log(hello('Sam'));
+console.log(hello("Sam", 30));
+
+// undefined를 명시적으로 전달해줘야한다.
+console.log(hello2(undefined, "sam"));
