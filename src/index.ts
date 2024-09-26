36adb9e2d2964e96d296c8dae516ba555ee16c84
[nyun-nye] <[2001_11_7@naver.com]>
2024-09-26 16:37:54 +0900

    🚩: interface의 optional, readonly

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,22 @@
-// user 객체 생성
-let user:object; // 객체는 object 타입
+// interface
+// optional, readonly
 
-user = {
-  name: 'xx',
-  age: 30
+// 프로퍼티 정의해서 객체 사용하고자 할 때는 interface 사용
+interface User{
+  name : string;
+  age : number;
+  gender? : string; // ?는 입력을 해도 되고 안해도된다 => optional
+  readonly birthYear : number; // readonly는 읽기 전용 속성
 }
 
-// console.log(user.name) // 에러 발생
+let user : User = {
+  name : 'xx',
+  age : 30,
+  birthYear : 2000
+}
+
+user.age=10;
+user.gender = 'male';
+
+console.log(user.age);
+
