e903d77290303ac22a1cf0aae54037fa0bbe2219
[nyun-nye] <[2001_11_7@naver.com]>
2024-09-26 20:48:23 +0900

    🚩 : 선택적 매개변수

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
-// src/index.ts
-const message: string = "Hello, TypeScript!";
-console.log(message);
+// 함수
+
+function hello(name?: string){ 
+  // 함수 파라미터로 optional 가능 => 선택적 매개변수
+  return `Hello, ${name || "world"}`;
+}
+
+// hello와 모습이 같은 함수
+function hello2(name = `world`){
+  return `Hello ${name}`;
+}
