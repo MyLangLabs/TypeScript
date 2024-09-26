424fcfec2f2b6b7bcb0c4e036a918e360d19b9f3
[nyun-nye] <[2001_11_7@naver.com]>
2024-09-26 16:38:53 +0900

    🚩 : optional 추가

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 // interface
-// optional, readonly
 
 // 프로퍼티 정의해서 객체 사용하고자 할 때는 interface 사용
 interface User{
   name : string;
   age : number;
-  gender? : string; // ?는 입력을 해도 되고 안해도된다 => optional
+  gender? : string; // ?는 입력을 해도 되고 안해도되고 => optional
   readonly birthYear : number; // readonly는 읽기 전용 속성
+  1? : string;
+  2? : string;
+  3? : string;
+  4? : string;
 }
 
 let user : User = {
@@ -19,4 +22,3 @@ user.age=10;
 user.gender = 'male';
 
 console.log(user.age);
-
