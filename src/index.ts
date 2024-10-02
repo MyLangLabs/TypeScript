1fab994c4da6ffe532ec2975e3d9f0558ef8c687
[nyun-nye] <[2001_11_7@naver.com]>
2024-10-02 22:48:21 +0900

    🚩: NonNullable<Type>

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
-// Exclude<T1, T2>
+// NonNullable<Type>
+// Null과 undefind를 제외한 타입 생성
 
-// T1에서 T2를 제외하고 사용
-// Omit은 프로퍼티를 제거, Exclude는 타입을 제거
-
-type T1 = string|number;
-type T2 = Exclude<T1,number>;
+type T1 = string | null | undefined | void;
+type T2 = NonNullable<T1>;
