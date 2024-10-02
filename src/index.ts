640d14b0400b56ee7b8255713fa407e911e90a35
[nyun-nye] <[2001_11_7@naver.com]>
2024-10-02 12:49:37 +0900

    🚩: class 사용시 멤버변수 선언

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
 // src/index.ts
-const message: string = "Hello, TypeScript!";
-console.log(message);
+class Car {
+  color: string; // ts에서 class를 사용하려면 멤버변수 미리 선언
+  constructor(color: string){
+    this.color = color;
+  }
+  start(){
+    console.log("start");
+  }
+}
+
+const bmw = new Car("red");
