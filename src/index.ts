d8435d7e4d284f1bbd02d797732eb653b1d14e0e
[nyun-nye] <[2001_11_7@naver.com]>
2024-10-02 15:09:47 +0900

    🚩: 제네릭을 사용한 인터페이스

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,23 @@
 // Generic
-// class, function, interface를 다양한 타입으로 재사용할 수 있다.
-// 선언할때는 타입 파라미터만 작성 -> 생성 시점에 사용할 타입 결정
 
-function getSize<T>(arr: T[]):number{
-  return arr.length;
+interface Mobile<T> {
+  name: String;
+  price: number;
+  option: T; // 값 미정
 }
 
-const arr1 = [1,2,3];
-getSize<number|string>(arr1); 
-// 특정 타입으로 강제하고 싶을 때 <>안에 값 넣어서 사용
+// const m1: Mobile<object> ={
+const m1: Mobile<{color: string; coupon: boolean}> = {
+  name: "s21",
+  price: 1000,
+  option:{
+    color: "red",
+    coupon: false,
+  },
+}
 
-const arr2 = ["a", "b", "c"];
-getSize<string>(arr2); // 3
-
-const arr3 = [false, true, false];
-getSize<boolean>(arr3); // 3
-
-const arr4 = [{}, {}, {name: "Tim"}];
-getSize(arr4); // 3
+const m2: Mobile<string> ={
+  name: "s20",
+  price: 900,
+  option: "good",
+};
