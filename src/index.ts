93e462bc69e97b24357cc90e796c511fc946a1d4
[nyun-nye] <[2001_11_7@naver.com]>
2024-10-02 20:07:11 +0900

    🚩: keyof 키워드 사용하기

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
-// src/index.ts
-const message: string = "Hello, TypeScript!";
-console.log(message);
+// keyof
+
+interface User{
+  id: number;
+  name: string;
+  age: number;
+  gender: "m" | "f";
+}
+
+type USerKey = keyof User;
+// keyof 키워드를 사용해서 키값들을 union 형태로 받을 수 있다.
+// 'id' | 'name' | 'age' | 'gender'
+
+const uk:USerKey = "id";
